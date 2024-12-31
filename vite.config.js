767387b54bce4dbd22bbc623bcdc8d54b75c6ea7
anchal-github123 <anchalnayak2003@gmail.com>
2024-12-31 16:54:11 +0530

    Done

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ReactMovieApp/', // Set this to your repo name on GitHub
+  build: {
+    outDir: 'build' // Ensure the build folder is used for GitHub Pages
+  }
+  
 })
